27f0397b4511b71a44d93e6edfa51061fbde40e7
Kostiantyn Gavura <kostya.gavura@gmail.com>
2021-09-23 13:27:28 +0200

    use promo link

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,11 +1,11 @@
-from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
+from pages.locators import MainPageLocators
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
 
     def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+        self.browser.find_element(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
