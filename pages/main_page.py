c275573afddd4225d082ffbe51d95b4fccf4f8b4
Kostiantyn Gavura <kostya.gavura@gmail.com>
2021-09-23 13:16:42 +0200

    add test whether guest see login link or not

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,11 @@
 from selenium.webdriver.common.by import By
-
-from .base_page import BasePage
+from pages.base_page import BasePage
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        self.browser.find_element(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
