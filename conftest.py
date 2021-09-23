b0989aa38287e9910741688bfc04a96392609174
Kostiantyn Gavura <kostya.gavura@gmail.com>
2021-09-23 12:03:18 +0200

    add default parameter for language

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store')
+    parser.addoption('--language', action='store', default='en')
 
 
 @pytest.fixture(scope='function')
