5d57b3d25e0fd3051d0cc7870d198b3096b60ded
Kostiantyn Gavura <kostya.gavura@gmail.com>
2021-09-23 15:54:43 +0200

    Switching between pages variant 2

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,13 +1,11 @@
 from pages.base_page import BasePage
 from pages.locators import MainPageLocators
-from pages.login_page import LoginPage
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
         link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         link.click()
-        return LoginPage(browser=self.browser, url=self.browser.current_url)
 
     def should_be_login_link(self):
         self.browser.find_element(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
