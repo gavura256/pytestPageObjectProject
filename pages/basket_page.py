4d96b468dd1ab8ba4534367f20600a864dff28fd
Kostiantyn Gavura <kostya.gavura@gmail.com>
2021-09-24 21:26:17 +0200

    test_guest_cannot_see_product_in_basket_opened_from_main_page v1

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -5,7 +5,7 @@ from pages.locators import BasketPageLocators
 
 class BasketPage(BasePage):
     def should_see_empty_basket_message(self):
-        self.browser.execute_script("$('#myfav').children().length > 0"),
+        assert  not self.browser.execute_script("$('#myfav').children().length > 0"), "Basket message isn't empty"
 
     def should_be_empty_basket(self):
         assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS), "Basket is not empty"
--- tests/test_basket_page.py
+++ tests/test_basket_page.py
@@ -4,7 +4,7 @@ from pages.basket_page import BasketPage
 from pages.main_page import MainPage
 
 
-LINK = "http://selenium1py.pythonanywhere.com/"
+LINK = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
 
 
 def test_guest_cannot_see_product_in_basket_opened_from_main_page(browser):
@@ -13,4 +13,5 @@ def test_guest_cannot_see_product_in_basket_opened_from_main_page(browser):
     page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty_basket()
-    basket_page.should_see_empty_basket_message(browser)
+    time.sleep(19)
+    basket_page.should_see_empty_basket_message()
