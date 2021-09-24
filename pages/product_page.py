7e03cb1eeae20336d1df17c10dc37acf3a6f265d
Kostiantyn Gavura <kostya.gavura@gmail.com>
2021-09-24 16:36:14 +0200

    final corrections for xfail examples

--- pages/product_page.py
+++ pages/product_page.py
@@ -20,5 +20,10 @@ class ProductPage(BasePage):
         assert product_price == message_text, "Wrong product's price in message"
 
     def should_not_be_success_message(self):
-        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+        assert not self.is_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
             "Success message is presented, but should not be"
+
+    def success_message_should_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,9 +1,7 @@
-import time
-
 import pytest
-
 from pages.product_page import ProductPage
-from pages.locators import ProductPageLocators
+
+LINK = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -23,25 +21,30 @@ def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_the_backed()
-    time.sleep(60)
     page.should_be_message_the_product_has_been_added_to_the_backed()
     page.price_should_be_as_expected()
 
 
 @pytest.mark.xfail
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_cannot_see_success_message_after_adding_product_to_basket(browser, link):
+def test_guest_cannot_see_success_message_after_adding_product_to_basket(browser):
     browser.delete_all_cookies()
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, LINK)
     page.open()
     page.add_product_to_the_backed()
     page.should_not_be_success_message()
+
+
+def test_guest_cannot_see_success_message(browser):
+    browser.delete_all_cookies()
+    page = ProductPage(browser, LINK)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basked(browser):
+    browser.delete_all_cookies()
+    page = ProductPage(browser, LINK)
+    page.open()
+    page.add_product_to_the_backed()
+    page.success_message_should_disappeared()
