c9f5866dd2817d5094f604c7bde8415bde300b07
Kostiantyn Gavura <kostya.gavura@gmail.com>
2021-09-24 12:56:06 +0200

    complete task: adding to the cart from the product page

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,5 +1,3 @@
-# pr
-
 from pages.product_page import ProductPage
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
@@ -9,6 +7,5 @@ def test_guest_can_add_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_the_backed()
-    # time.sleep(999)
     page.should_be_message_the_product_has_been_added_to_the_backed()
     page.price_should_be_as_expected()
