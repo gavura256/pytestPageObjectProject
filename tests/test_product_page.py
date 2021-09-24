dc1ffa4624d8f477cf193654140e34dc9bd0a22a
Kostiantyn Gavura <kostya.gavura@gmail.com>
2021-09-24 15:07:05 +0200

    added test_guest_cant_see_success_message_after_adding_product_to_basket with xfail mark

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -3,6 +3,7 @@ import time
 import pytest
 
 from pages.product_page import ProductPage
+from pages.locators import ProductPageLocators
 
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -22,5 +23,25 @@ def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_the_backed()
+    time.sleep(60)
     page.should_be_message_the_product_has_been_added_to_the_backed()
     page.price_should_be_as_expected()
+
+
+@pytest.mark.xfail
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser, link):
+    browser.delete_all_cookies()
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_the_backed()
+    page.should_not_be_success_message()
