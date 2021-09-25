c60bf19832abccfb60dca4bfb3e762f60dd59e4f
Kostiantyn Gavura <kostya.gavura@gmail.com>
2021-09-25 15:10:28 +0200

    add class TestUserAddToBasketFromProductPage and tests

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,9 +1,33 @@
 import pytest
 
+from pages.basket_page import BasketPage
 from pages.login_page import LoginPage
+from pages.main_page import MainPage
 from pages.product_page import ProductPage
 
 LINK = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+
+
+class TestUserAddToBasketFromProductPage:
+    def test_user_cannot_see_product_in_basket_opened_from_main_page(self, browser):
+        browser.delete_all_cookies()
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_basket_page()
+        basket_page = BasketPage(browser, browser.current_url)
+        basket_page.should_be_empty_basket()
+        basket_page.should_see_empty_basket_message()
+
+    def test_user_cannot_see_product_in_basket_opened_from_product_page(self, browser):
+        browser.delete_all_cookies()
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_basket_page()
+        basket_page = BasketPage(browser, browser.current_url)
+        basket_page.should_be_empty_basket()
+        basket_page.should_see_empty_basket_message()
 
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
