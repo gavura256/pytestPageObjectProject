6c0bc432dff308d5492b9a847361ff2f79bd0868
Kostiantyn Gavura <kostya.gavura@gmail.com>
2021-09-25 15:09:34 +0200

    grouped tests to the class TestLoginFromMainPage

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,25 +1,27 @@
-# pytest -v --tb=line --language=fr test_main_page.py
+# pytest -v --tb=line --language=fr test_main_page.py -m login_guest
+import pytest
+
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+    def test_guest_can_go_to_login_page(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
-def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
-
-
-def test_guest_should_see_login_page(browser):
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_page()
+    def test_guest_should_see_login_page(self, browser):
+        page = LoginPage(browser, link)
+        page.open()
+        page.should_be_login_page()
