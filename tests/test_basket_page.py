07975298fa3f9e69c22bc07fe40c8d55904bcb7f
Kostiantyn Gavura <kostya.gavura@gmail.com>
2021-09-25 13:49:07 +0200

    added test_guest_cannot_see_product_in_basket_opened_from_product_page

--- tests/test_basket_page.py
+++ tests/test_basket_page.py
@@ -1,13 +1,21 @@
-import time
-
 from pages.basket_page import BasketPage
 from pages.main_page import MainPage
-
-LINK = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+from pages.product_page import ProductPage
 
 
 def test_guest_cannot_see_product_in_basket_opened_from_main_page(browser):
-    page = MainPage(browser, LINK)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket()
+    basket_page.should_see_empty_basket_message()
+
+
+def test_guest_cannot_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
     page.open()
     page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)
