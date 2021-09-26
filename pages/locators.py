f856fc4d0fc8e47d81b36b2735a3be1c2542886d
Kostiantyn Gavura <kostya.gavura@gmail.com>
2021-09-26 20:37:07 +0200

    deleted unneeded locator

--- pages/locators.py
+++ pages/locators.py
@@ -9,7 +9,6 @@ class BasketPageLocators:
 class BasePageLocators:
     USER_ICON = (By.CSS_SELECTOR, ".icon-user")
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 
 class LoginPageLocators:
